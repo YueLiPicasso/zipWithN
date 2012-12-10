feb8c73f36a5694e7c5cf0ae685832311013b3e7
Takayuki Muranushi <muranushi@gmail.com>
2012-12-10 11:01:26 +0900

    move the directory to root.

--- test/doctest.hs
+++ test/doctest.hs
@@ -3,4 +3,4 @@ module Main where
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["-idist/build/autogen/", "./src/Data/Array/ZipWithN/Internal.hs"]
+main = doctest ["-idist/build/autogen/", "./Data/Array/ZipWithN/Internal.hs"]
